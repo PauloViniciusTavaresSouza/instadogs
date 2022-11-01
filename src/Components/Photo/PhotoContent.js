93db9fcbcf51f43cd28e79f7b62f7bc81808298c
Paulo Vinicius <pauloviniciustavaressouza@gmail.com>
2022-11-01 10:27:44 -0300

    Atualizacao do feed

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { Image } from '../Helper/Image';
 import { PhotoComments } from './PhotoComments';
 import styles from './PhotoContent.module.css';
+import { PhotoDelete } from './PhotoDelete';
 
 export const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   console.log('o que vem no data:', data);
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
--- src/api.js
+++ src/api.js
@@ -63,7 +63,7 @@ export function PHOTO_POST(formData, token) {
   };
 }
 
-export function PHOTOS_GET(page, total, user) {
+export function PHOTOS_GET({ page, total, user }) {
   return {
     url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
     options: {
@@ -93,6 +93,18 @@ export function COMMENT_POST(id, body) {
         Authorization: 'Bearer' + window.localStorage.getItem('token'),
       },
       body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: `${API_URL}/api/PHOTO/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer' + window.localStorage.getItem('token'),
+      },
     },
   };
 }
